07d5be1f87b05ce5316294535c2862589ecbea64
Your Name <you@example.com>
2022-02-08 14:36:23 +0530

    add Button Added

--- components/Loginpage.js
+++ components/Loginpage.js
@@ -2,13 +2,23 @@ import { StyleSheet, Text, View,Image, TouchableOpacity } from 'react-native';
 import * as Google from 'expo-google-app-auth';
 import MainPage from './MainPage/FlatlistUI';
 import React from 'react';
+import axios from 'axios';
+import * as SecureStore from 'expo-secure-store';
+
+import {IP_ADD} from '@env'
 
 export default class LoginPage extends React.Component {
   constructor(props) {
     super(props)
     this.state = {signedIn: false, name: "", photoUrl: "",email:""}
     this.HandleClick=this.HandleClick.bind(this);
-    }
+    this.saveToken = this.saveToken.bind(this);
+  }
+
+  saveToken = async(key, value) => {
+    await SecureStore.setItemAsync(key, value)
+  }
+
     //Function to handle onPress.
      HandleClick = async() =>{
       try {
@@ -16,13 +26,30 @@ export default class LoginPage extends React.Component {
           androidClientId: "819068882370-ug04p1qchq01hsqn57q36vshm26i1doe.apps.googleusercontent.com",
           scopes: ["profile", "email"]
         })
+        console.log("Result.user", result.user)
         if (result.type === "success") {
-          this.setState({
-            signedIn: true,
-            email:result.user.email,
-            name: result.user.name,
-            photoUrl: result.user.photoUrl
-          });
+          const { id, email, givenName, familyName, photoUrl, name } = result.user;
+          const data = {
+            email,
+            firstName: givenName,
+            lastName: familyName,
+            avatar: photoUrl,
+            googleId: id
+          }
+          axios.post(`${IP_ADD}/user/login`, data)
+            .then(async (response) => {
+              if(response.data.success) {
+                this.setState({
+                  signedIn: true,
+                  email:email,
+                  name: name,
+                  photoUrl: photoUrl
+                });
+
+                // Save token in local storage
+                this.saveToken("token", response.data.token);
+              }
+            })
         } else {
           console.log("cancelled")
         }
--- components/MainPage/FlatlistUI/index.js
+++ components/MainPage/FlatlistUI/index.js
@@ -1,49 +1,30 @@
-import { View, Text, Image,StyleSheet,TouchableOpacity } from 'react-native';
+import { View, Text, Image,StyleSheet } from 'react-native';
 import React from 'react';
 //This is just a demo for the Main Page UI 
 const  MainPage = (props) => {
   return (
     <View>
+    <Text style={styles.header}>Welcome:{props.name}</Text>
     <Image style={styles.image} source={{ uri: props.photoUrl }}/>
-    <Text style={styles.header}>SITS Confessions</Text>
-    <TouchableOpacity
-        onPress={() => console.log("Add Form")}
-    >
-        <Image
-            style={styles.addButton}
-            source={require('../../../assets/add.png')}
-         />
-    </TouchableOpacity>
     </View>
   );
 }
 const styles= StyleSheet.create({
     header: {
         alignSelf:'center',
-        fontSize: 35,
-        marginTop:-42,
-        marginStart:25,
-        fontWeight:'bold',
+        fontSize: 25,
+        marginTop:150,
         alignItems:'center',
         color:'black'
     },
     image: {
-        marginStart:10,
-        marginTop:50,
-        width: 40,
-        height: 40,
+        alignSelf:'center',
+        marginTop: 15,
+        width: 150,
+        height: 150,
         borderColor: "rgba(0,0,0,0.2)",
         borderWidth: 3,
         borderRadius: 150
-    },
-    addButton:{
-        marginTop: 635,
-        width: 65,
-        marginStart:300,
-        height: 55,
-        alignContent:'center',
-        padding: 10,
-        borderRadius: 100,
-    },
+    }
 })
 export default MainPage;
