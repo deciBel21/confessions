e1fd5fc0f66b915a8f1bba908d5344b42ff92259
vighnesh192 <vighnesh192000@gmail.com>
2022-03-31 21:10:18 +0530

    Fix: Properties names changed for GoogleSignIn in for Prod

--- components/Loginpage.js
+++ components/Loginpage.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View,Image, TouchableOpacity, ToastAndroid } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Alert } from 'react-native';
 import * as Google from 'expo-google-app-auth';
 import * as GoogleSignIn from 'expo-google-sign-in';
 import MainPage from './MainPage/FlatlistUI';
@@ -6,11 +6,11 @@ import React from 'react';
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 import analytics from '@react-native-firebase/analytics';
- 
+
 export default class LoginPage extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {signedIn: false, name: "", photoUrl: "",email:""}
+    this.state = { signedIn: false, name: "", photoUrl: "", email: "" }
     this.HandleClick = this.HandleClick.bind(this);
     this.saveToken = this.saveToken.bind(this);
     this.initAsync = this.initAsync.bind(this);
@@ -25,19 +25,43 @@ export default class LoginPage extends React.Component {
       })
       this.getUserDetails();
     } catch (error) {
-        ToastAndroid.show(`INIT ASYNC ERROR:- ${error}`, ToastAndroid.LONG)
-        console.log("INIT ASYNC ERROR:- ", error)
+      Alert.alert(
+        "INIT ASYNC ERROR:-",
+        JSON.stringify(error),
+        [
+          {
+            text: "Cancel",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel"
+          },
+          { text: "OK", onPress: () => console.log("OK Pressed") }
+        ]
+      );
+      console.log("INIT ASYNC ERROR:- ", error)
     }
   }
 
   getUserDetails = async () => {
     try {
       const user = await GoogleSignIn.signInSilentlyAsync();
-      if(user) this.setState({
+      if (user) this.setState({
+        photoUrl: user.photoURL,
         signedIn: true
       });
     } catch (error) {
-        console.log("Google sign in error:- ", error)
+      Alert.alert(
+        "signInSilentlyAsync() ERROR:-",
+        JSON.stringify(error),
+        [
+          {
+            text: "Cancel",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel"
+          },
+          { text: "OK", onPress: () => console.log("OK Pressed") }
+        ]
+      );
+      console.log("Google sign in error:- ", error)
     }
   }
 
@@ -45,7 +69,7 @@ export default class LoginPage extends React.Component {
     this.initAsync();
   }
 
-  saveToken = async(key, value) => {
+  saveToken = async (key, value) => {
     await SecureStore.setItemAsync(key, value)
   }
 
@@ -53,7 +77,7 @@ export default class LoginPage extends React.Component {
   HandleClick = async () => {
     try {
       let result;
-      if(!__DEV__) {
+      if (!__DEV__) {
         // await analytics().logLogin({
         //   method: 'google'
         // })
@@ -69,22 +93,22 @@ export default class LoginPage extends React.Component {
         console.log("Result.user", result.user)
       }
       if (result.type === "success") {
-        const { id, email, givenName, familyName, photoUrl, name } = result.user;
+        const { uid, id, email, givenName, familyName, photoUrl, photoURL, firstName, lastName, name } = result.user;
         const data = {
           email,
-          firstName: givenName,
-          lastName: familyName,
-          avatar: photoUrl,
-          googleId: id
+          firstName: __DEV__ ? givenName : firstName,
+          lastName: __DEV__ ? familyName : lastName,
+          avatar: __DEV__ ? photoUrl : photoURL,
+          googleId: __DEV__ ? id : uid
         }
         axios.post(`${this.props.URL}/user/login`, data)
           .then(async (response) => {
-            if(response.data.success) {
+            if (response.data.success) {
               this.setState({
                 signedIn: true,
-                email:email,
-                name: name,
-                photoUrl: photoUrl
+                email: email,
+                name: `${firstName} ${lastName}`,
+                photoUrl: __DEV__ ? photoUrl : photoURL
               });
 
               // Save token in local storage
@@ -96,75 +120,86 @@ export default class LoginPage extends React.Component {
         console.log("cancelled")
       }
     } catch (e) {
-      ToastAndroid.show(`HANDLE CLICK ERROR:- ${e}`, ToastAndroid.LONG)
+      Alert.alert(
+        "HANDLE CLICK ERROR:-",
+        JSON.stringify(e),
+        [
+          {
+            text: "Cancel",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel"
+          },
+          { text: "OK", onPress: () => console.log("OK Pressed") }
+        ]
+      );
       console.log("error", e)
     }
   }
 
-  render(){
-  return (
-    <View >
-    {this.state.signedIn ? (
-      // User verfied => Land to main page.
-      console.log(`User Loged In! with name: ${this.state.name} , email: ${this.state.email} and photoUrl: ${this.state.photoUrl  }`),
-      //Navigate user to the Flast List for now the MainPage which is the UI component for the Flatlist.
-      <MainPage URL={this.props.URL} name= {this.state.name} photoUrl={this.state.photoUrl}/>
-    ) : (
-      //If not veified => loginPage.
-      <View>
-      <Text style={styles.introText}>Welcome to STES Confessions!</Text>
-      <TouchableOpacity
-        onPress={this.HandleClick}
-        style={styles.roundButton1}>
-        <Image source={require('../assets/gicon.png')}
-        ></Image>
-      </TouchableOpacity>
-      <Text  onPress={this.HandleClick}
-      style={styles.loginText}>To continue, please {"signup."}</Text>
-      <Text style={styles.copyright}>
-          © 2022 DK Tech.
-      </Text>
+  render() {
+    return (
+      <View >
+        {this.state.signedIn ? (
+          // User verfied => Land to main page.
+          console.log(`User Loged In! with name: ${this.state.name} , email: ${this.state.email} and photoUrl: ${this.state.photoUrl}`),
+          //Navigate user to the Flast List for now the MainPage which is the UI component for the Flatlist.
+          <MainPage URL={this.props.URL} name={this.state.name} photoUrl={this.state.photoUrl} />
+        ) : (
+          //If not veified => loginPage.
+          <View>
+            <Text style={styles.introText}>Welcome to STES Confessions!</Text>
+            <TouchableOpacity
+              onPress={this.HandleClick}
+              style={styles.roundButton1}>
+              <Image source={require('../assets/gicon.png')}
+              ></Image>
+            </TouchableOpacity>
+            <Text onPress={this.HandleClick}
+              style={styles.loginText}>To continue, please {"signup."}</Text>
+            <Text style={styles.copyright}>
+              © 2022 DK Tech.
+            </Text>
+          </View>
+        )}
       </View>
-    )}
-    </View>
-  );
+    );
   }
 };
 //Styling.
 const styles = StyleSheet.create({
-    introText:{
-        color:'black',
-        fontSize:38,
-        padding:1,
-        marginTop:125,
-        alignContent:'center',
-        marginStart:20,
-    },
-    roundButton1:{
-        marginTop: 300,
-        width: 50,
-        height: 150,
-        alignContent:'center',
-        alignSelf:'center',
-        alignItems: 'center',
-        padding: 10,
-        borderRadius: 100,
-    },
-    loginText:{
-      color:'black',
-      alignSelf:'center',
-      fontSize:15,
-      opacity:0.4,
-      marginTop:-70,
-      fontWeight:'bold',
-      textDecorationLine:'underline',
-    },
-    copyright:{
-      color:'black',
-      alignSelf:'center',
-      position:'absolute',
-      marginTop:800,
-      fontWeight:'bold',
-      fontSize:15,
-    }
+  introText: {
+    color: 'black',
+    fontSize: 38,
+    padding: 1,
+    marginTop: 125,
+    alignContent: 'center',
+    marginStart: 20,
+  },
+  roundButton1: {
+    marginTop: 300,
+    width: 50,
+    height: 150,
+    alignContent: 'center',
+    alignSelf: 'center',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 100,
+  },
+  loginText: {
+    color: 'black',
+    alignSelf: 'center',
+    fontSize: 15,
+    opacity: 0.4,
+    marginTop: -70,
+    fontWeight: 'bold',
+    textDecorationLine: 'underline',
+  },
+  copyright: {
+    color: 'black',
+    alignSelf: 'center',
+    position: 'absolute',
+    marginTop: 800,
+    fontWeight: 'bold',
+    fontSize: 15,
+  }
 });
